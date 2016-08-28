13499e46ef1ed2eb84edb77bd80a2422046925d5
Tadas Radvilavičius <radvilt@gmail.com>
2016-08-28 16:28:51 +0300

    Examples fix

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -4,6 +4,7 @@ var Examples = (props) => {
   return (
     <h3> Examples component! </h3>
     <p> Welcome to Examples page!</p>
+    <p> Something awesome will be available here soon</p>
   );
 };
 
