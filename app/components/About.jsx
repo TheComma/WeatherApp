72fdbee89c9f62c6fea2b41df542a82351813039
Tadas Radvilavičius <radvilt@gmail.com>
2016-08-29 12:36:54 +0300

    Style update

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,19 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-    <h3> About </h3>
-    <p> Welcome to the About page!</p>
+      <h1 className="text-center"> About </h1>
+      <p> This is a weather application. You can use this application
+        for checking weather in various cities around the world.</p>
+      <p> Simply type a city name in the search bar and click 'Get Weather'</p>
+      <p> Tools that are beeing used in this app: </p>
+      <ul>
+        <li>
+          <a href="https://facebook.github.io/react">React</a> - Javascript framework
+        </li>
+        <li>
+          <a href="http://openweathermap.org">Open Weather Map</a> - API for information about weather
+        </li>
+      </ul>
     </div>
   );
 };
--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -5,7 +5,7 @@ var WeatherMessage = (props) =>{
   var temp = props.temp;
   return (
       <div>
-        <p>It is {temp} in the {location} right now!</p>
+        <p className="text-center">It is {temp} in the {location} right now!</p>
       </div>
   );
 }
